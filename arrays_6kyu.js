fa2c9447f11ada39045f0cffc81639b195ad7baf
Bejan <bmaljai@gmail.com>
2022-04-18 14:45:45 -0700

    add arrays_6kyu file to start on new problems

---
+++ arrays_6kyu.js
@@ -0,0 +1,44 @@
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+
+  function createPhoneNumber(numbers){
+    numbers = numbers.join('');
+    return '(' + numbers.substring(0, 3) + ') ' 
+        + numbers.substring(3, 6) 
+        + '-' 
+        + numbers.substring(6);
+  }
+
+  function createPhoneNumber(numbers){
+    return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+  }
+
+  function createPhoneNumber(numbers){
+    return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
+ }
+
+ function createPhoneNumber(numbers){
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+  }
+
+
+
+
+
+  function createPhoneNumber(numbers){
+    var format = "(ooo) ooo-oooo";
+    numbers.forEach(item=>{
+      format = format.replace(
+        'o', item
+      );
+    });
+    return format;
+  }
