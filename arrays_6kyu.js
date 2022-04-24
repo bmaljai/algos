363070ae9e92b4de93fcaa20e1fb35122a6f6306
Bejan <bmaljai@gmail.com>
2022-04-23 22:45:18 -0700

    addition 4/23

--- arrays_6kyu.js
+++ arrays_6kyu.js
@@ -54,3 +54,36 @@ function createPhoneNumber(numbers){
     list = [list.filter(a=>a.language==='Python').length,list.filter(a=>a.language==='Ruby').length,list.filter(a=>a.language==='JavaScript').length]
     return Math.max.apply(null,list)/Math.min.apply(null,list) <= 2
   }
+
+
+  //https://www.codewars.com/kata/514b92a657cdc65150000006/solutions/javascript
+  function solution(number){
+    var n3 = Math.floor(--number/3), n5 = Math.floor(number/5), n15 = Math.floor(number/15);
+    return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15+1)) /2;
+  }
+
+  function solution(number){
+    var sum = 0;
+    
+    for(var i = 1;i< number; i++){
+      if(i % 3 == 0 || i % 5 == 0){
+        sum += i
+      }
+    }
+    return sum;
+  }
+
+  //mine
+  function solution(number){
+    if(number<0){return 0}
+    let numarr = [];
+    for(let i=1;i<number;i++){
+      if (i%15===0){numarr.push(i)}
+      else{
+      if (i%3===0){numarr.push(i)}
+      if (i%5===0){numarr.push(i)}
+      }
+  
+    }
+    return numarr.reduce((acc,curr)=>acc+=curr,0);
+  }
