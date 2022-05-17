bf95118178abf4dd13df26d4f5decbdfbc2fa08c
Bejan Maljai <bmaljai@gmail.com>
2022-05-16 20:54:33 -0700

    5/16/2022

---
+++ 5_16_2022.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
+function squareDigits(num){
+    return Number(String(num).split('').map(item=>String((+item)**2)).join(''))
+  }
