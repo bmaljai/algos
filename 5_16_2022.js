efe90b7b3027f3b9a0c9e3ba47a1cc6a3f8c9df7
Bejan Maljai <bmaljai@gmail.com>
2022-05-20 08:20:23 -0700

    5/20

--- 5_16_2022.js
+++ 5_16_2022.js
@@ -6,3 +6,21 @@ function squareDigits(num){
 function past(h, m, s) {
   return h * 3600000 + m * 60000 + s * 1000
 }
+
+function duplicateEncode(word){
+  word = word.toLowerCase();
+  let obj = {};
+  for(let i =0;i<word.length;i++){
+    if(word[i] in obj){
+       obj["" + word[i]]=")";
+    }else{
+      obj[String(word[i])]="(";
+    }
+  }
+  let newstr = "";
+  for(let j=0;j<word.length;j++){
+    newstr += obj[word[j]]
+  }
+return newstr;
+  
+}
