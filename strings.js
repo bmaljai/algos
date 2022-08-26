e7657cdbfa1d9656954f9e208e7f7d22cd601fa9
Bijan Maljai <bmaljai@gmail.com>
2022-08-25 20:39:28 -0700

    august 25 2022

--- strings.js
+++ strings.js
@@ -7,3 +7,15 @@ function towerBuilder(nFloors) {
     }
     return tower;
 }
+
+function isIsogram(str) {
+    str = str.toLowerCase()
+
+    for (var i = 0; i < str.length; i++) {
+        if (str.indexOf(str.charAt(i), i + 1) >= 0) {
+            return false
+        }
+    }
+
+    return true
+}
