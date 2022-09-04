8f37732a68eb9a9902372cded7b7c751d9d72d38
Bijan Maljai <bmaljai@gmail.com>
2022-09-03 20:33:11 -0700

    sep 3 2022

--- strings.js
+++ strings.js
@@ -125,3 +125,16 @@ function solution(str) {
     }
     return newstr.split(',')
 }
+
+function solution(str) {
+    var i = 0;
+    var result = new Array();
+    if (str.length % 2 !== 0) {
+        str = str + '_';
+    }
+    while (i < str.length) {
+        result.push(str[i] + str[i + 1]);
+        i += 2;
+    }
+    return result;
+}
