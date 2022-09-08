c812a1d9e69c23521f70db94249de667437b31bd
Bijan Maljai <bmaljai@gmail.com>
2022-09-08 10:13:39 -0700

    spet 8 2022

--- arrays.js
+++ arrays.js
@@ -8,3 +8,10 @@ const binaryArrayToNumber = arr => {
     })
     return total
 };
+
+function getAverage(marks) {
+    //TODO : calculate the downward rounded average of the marks array
+    return Math.floor(marks.reduce((acc, current) =>
+        acc + current, 0) / marks.length)
+
+}
